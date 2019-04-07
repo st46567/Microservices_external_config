6fff7444a19d78279457822dd59eefd8e82034c1
Jan Houzvicka <st46567@student.upce.cz>
2019-04-07 19:41:20 +0200

    Microservices - external config

---
+++ eshop_microservice_configServer/src/main/java/cz/houzvicka_jan/demo_eshop/ConfigServiceMain.java
@@ -0,0 +1,15 @@
+package cz.houzvicka_jan.demo_eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServiceMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceMain.class, args);
+    }
+
+}
